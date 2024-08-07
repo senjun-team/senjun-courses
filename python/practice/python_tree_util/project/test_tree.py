dce89422234830a2c282e4ed28dcbf5ade0938c3
Olga Khlopkova <mesozoic.drones@gmail.com>
2024-08-07 15:25:40 +0300

    Review fixes

--- python/practice/python_tree_util/project/test_tree.py
+++ python/practice/python_tree_util/project/test_tree.py
@@ -1,15 +1,16 @@
-import unittest
+import os
 import subprocess
+import unittest
 from pathlib import Path
 
 
-def run_tree_util(args):
+def run_tree_util(util_path, args):
     """
     Запускает скрипт tree.py и возвращает его консольный вывод
     в виде строки
     """
 
-    cmd_line_args = ["python3", "tree.py", *args]
+    cmd_line_args = ["python3", os.path.join(util_path, "tree.py"), *args]
     res = subprocess.run(cmd_line_args, stdout=subprocess.PIPE)
     return res.stdout.decode("utf-8")
 
@@ -24,8 +25,8 @@ class TestTree(unittest.TestCase):
         # и аргументов для запуска скрипта tree.py.
         self.cases = [
             ("dir without args", FULL_DIR, ["test_data"]),
-            ("dir with -d", FULL_DIR_DIR_ONLY, ["./test_data/", "-d"]),
-            ("dir with -L 1", DIR_LEVEL_1, ["test_data/", "-L 1"]),
+            ("dir with -d", FULL_DIR_DIR_ONLY, ["-d", "./test_data/"]),
+            ("dir with -L 1", DIR_LEVEL_1, ["-L 1", "test_data/"]),
             (
                 "dir with -L 2 -d",
                 DIR_LEVEL_2_DIR_ONLY,
@@ -40,24 +41,43 @@ class TestTree(unittest.TestCase):
             (
                 "subdir: fantasy, -L 1",
                 DIR_FANTASY_LEVEL_1,
-                ["./test_data/movies/fantasy/", "-L 1"],
+                ["-L 1", "./test_data/movies/fantasy/"],
             ),
             (
                 "subdir: LOTR",
                 DIR_LOTR,
                 ["test_data/movies/fantasy/LOTR"],
             ),
+            (
+                "not normalized path",
+                DIR_NOT_NORMALIZED,
+                ["./test_data/movies/../movies/sci-fy"],
+            ),
             ("empty dir", DIR_EMPTY, [str(self.empty_dir)]),
+        ]
+
+        self.cases_cur_dir = [
+            ("dir not set", DIR_CURRENT, []),
+            ("cur dir set explicitly", DIR_CURRENT, ["."]),
+            ("dir not set, -d", DIR_CURRENT_DIRS_ONLY, ["-d"]),
         ]
 
     def tearDown(self):
         self.empty_dir.rmdir()
 
-    def test_tree_util(self):
-        for test_name, plan_output, args in self.cases:
+    def run_test_cases(self, util_path, tests):
+        for test_name, plan_output, args in tests:
             with self.subTest(test_name):
-                fact_output = run_tree_util(args)
+                fact_output = run_tree_util(util_path, args)
                 self.assertEqual(fact_output, plan_output)
+
+    def test_tree_util(self):
+        self.run_test_cases("", self.cases)
+
+        cwd = os.getcwd()
+        os.chdir("test_data/movies/fantasy")
+        self.run_test_cases("../../..", self.cases_cur_dir)
+        os.chdir(cwd)
 
 
 FULL_DIR = """test_data
@@ -140,9 +160,31 @@ DIR_LOTR = """test_data/movies/fantasy/LOTR
 1 directory, 1 file
 """
 
+DIR_NOT_NORMALIZED = """./test_data/movies/../movies/sci-fy
+├── 12 Monkeys.avi
+└── Blade runner.avi
+
+1 directory, 2 files
+"""
+
 DIR_EMPTY = """test_data/movies/historical
 
 1 directory, 0 files
+"""
+
+DIR_CURRENT = """.
+├── Edward Scissorhands.avi
+├── LOTR
+│   └── The Two Towers.avi
+└── Time Bandits.avi
+
+2 directories, 3 files
+"""
+
+DIR_CURRENT_DIRS_ONLY = """.
+└── LOTR
+
+2 directories
 """
 
 if __name__ == "__main__":
