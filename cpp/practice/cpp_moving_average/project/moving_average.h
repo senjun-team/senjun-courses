e7864b3fb6e7a5885417e038d73b6b5d5d5bb138
Microvenator <116944723+Microvenator@users.noreply.github.com>
2025-05-03 08:35:52 +0300

    C++ practice 2 (#204)
    Co-authored-by: Microvenator <mesozoic.drones@gmail.com>

---
+++ cpp/practice/cpp_moving_average/project/moving_average.h
@@ -0,0 +1,32 @@
+#pragma once
+
+import std;
+
+#include <cmath> // содержит константу NAN
+
+class MovingAverage
+{
+public:
+    MovingAverage(std::size_t n)
+    {
+
+    }
+
+    std::size_t window_size()
+    {
+
+    }
+
+    void add(int number)
+    {
+
+    }
+
+    double val()
+    {
+
+    }
+
+private:
+
+};
---
+++ cpp/practice/cpp_moving_average/project_solution/moving_average.h
@@ -0,0 +1,52 @@
+#pragma once
+
+import std;
+
+#include <cmath> // содержит константу NAN
+
+class MovingAverage
+{
+public:
+    MovingAverage(std::size_t n)
+    {
+        if (n == 0)
+            throw std::invalid_argument("Window size must be > 0");
+        
+        size = n;
+    }
+
+    std::size_t window_size()
+    {
+        return size;
+    }
+
+    void add(int number)
+    {
+        if (numbers.size() == size)
+        {
+            sum -= numbers.front();
+            numbers.pop();
+        }
+
+        sum += number;
+        numbers.push(number);
+    }
+
+    double val()
+    {
+        if (numbers.size() < size)
+            return NAN;
+
+        return static_cast<double>(sum) / size;
+    }
+
+private:
+    // Значения в скользящем окне
+    std::queue<int> numbers;
+
+    // Сумма этих значений
+    int sum = 0;
+
+    // Размер окна
+    std::size_t size = 0;
+};
