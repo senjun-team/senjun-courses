9ac818fddb1ee2f253715227530befd4db2ced0a
korepanov <korepanov.viacheslav@yandex.ru>
2025-08-25 15:32:42 +0300

    [-] golang: calculator: cerrors

--- golang/practice/calculator/project_solution/internal/cerrors/cerrors.go
+++ golang/practice/calculator/project_solution/internal/cerrors/cerrors.go
@@ -3,15 +3,11 @@ package cerrors
 import "errors"
 
 var (
-	ErrNoToken     = errors.New("no token found")
-	ErrLexAnalysis = errors.New("lexical analysis failed")
-	ErrRead        = errors.New("reading program failed")
-	ErrCalculate   = errors.New("calculating failed")
-	ErrNoFactor    = errors.New("no factor")
-	ErrNoTerm      = errors.New("no term")
-	ErrNoExpr      = errors.New("no expression")
-	ErrParse       = errors.New("parsing failed")
-	ErrNoNode      = errors.New("no node")
-	ErrAppendNode  = errors.New("appending node failed")
-	ErrSolve       = errors.New("solve failed")
+	ErrNoToken    = errors.New("no token found")
+	ErrCalculate  = errors.New("calculating failed")
+	ErrNoFactor   = errors.New("no factor")
+	ErrParse      = errors.New("parsing failed")
+	ErrNoNode     = errors.New("no node")
+	ErrAppendNode = errors.New("appending node failed")
+	ErrSolve      = errors.New("solve failed")
 )
