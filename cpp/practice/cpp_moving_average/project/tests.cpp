36522efec0386481cd8ab40ba32deafb43c211ee
Microvenator <116944723+Microvenator@users.noreply.github.com>
2025-11-24 17:46:11 +0300

    Practice update for new C++ image (#296)
    * Practice 1. div without div
    
    * Div without div
    
    * Practice 2. Moving average
    
    * Practice 3. Brainfuck
    
    * Practice 4. LRU cache
    
    * Chapter 3. task 1 fix

--- cpp/practice/cpp_moving_average/project/tests.cpp
+++ cpp/practice/cpp_moving_average/project/tests.cpp
@@ -1,9 +1,85 @@
+#include <cmath> // содержит константу NAN
+
 import boost.ut;
-import test_helpers;
-
 import moving_average;
-
 import std;
+
+// senjun::MovingAverage - это эталонное решение, относительно которого
+// будет замеряться производительность решения пользователя.
+// Если вы не хотите случайно увидеть решение практики, не читайте этот файл.
+namespace senjun
+{
+    class MovingAverage
+    {
+    public:
+        MovingAverage(std::size_t n)
+        {
+            if (n == 0)
+                throw std::invalid_argument("Window size must be > 0");
+            
+            size = n;
+        }
+
+        std::size_t window_size()
+        {
+            return size;
+        }
+
+        void add(int number)
+        {
+            if (numbers.size() == size)
+            {
+                sum -= numbers.front();
+                numbers.pop();
+            }
+
+            sum += number;
+            numbers.push(number);
+        }
+
+        double val()
+        {
+            if (numbers.size() < size)
+                return NAN;
+
+            return static_cast<double>(sum) / size;
+        }
+
+    private:
+        // Значения в скользящем окне
+        std::queue<int> numbers;
+
+        // Сумма этих значений
+        int sum = 0;
+
+        // Размер окна
+        std::size_t size = 0;
+    };
+}
+
+// Среднее арифметическое значений вектора
+ template<class C>
+double average(std::vector<C> const& v)
+{
+    if(v.empty())
+        return 0;
+
+    return std::reduce(v.begin(), v.end()) / static_cast<double>(v.size());
+}
+
+// Получение вектора длины len, заполненного случайными числами от -100 до 200
+std::vector<int> random_vector(std::size_t len)
+{
+    std::random_device rnd_device;
+    std::mt19937 mersenne_engine {rnd_device()};
+    std::uniform_int_distribution<int> dist {-100, 200};
+    
+    auto gen = [&](){ return dist(mersenne_engine); };
+
+    std::vector<int> vec(len);
+    std::generate(vec.begin(), vec.end(), gen);
+    return vec;
+}
 
 using namespace boost::ut;
 
@@ -179,7 +255,7 @@ int main()
                      input.size() - window_size + 1,
                      window_size);
 
-        // Примем 30% за допустимое замедление, включая погрешность вычисления на VPS
-        expect(avg_user <= avg_senjun * 1.3) << "Your implementation of MovingAverage doesn't fit our timeouts. Please optimize it." << fatal;
+        // Допустимое замедление, включая погрешность вычисления на VPS
+        expect(avg_user <= avg_senjun * 3.0) << "Your implementation of MovingAverage doesn't fit our timeouts. Please optimize it." << fatal;
     };
 }
